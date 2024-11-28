449fd20393eb93601ddfa457a4bee2076d75ae52
Keun-Lemur <keunlemur@gmail.com>
2024-11-28 13:42:45 +0900

    ADD: Implement coke vending machine. getbalance, restock and purchase

---
+++ vendingmachine/contracts/vendingmachine.sol
@@ -0,0 +1,41 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+/*
+What is a VendingMachine? What are the functions of this VendingMachine?
+Functions:
+1. Purchase
+2. Restock
+3. Get balance
+State variables:
+1. owner
+2. balance
+*/
+
+
+contract VendingMachine {
+    address public owner; // The owner of the contract (coke vending machine)
+    mapping(address => uint) public cokeBalance; // The balance of the vending machine
+
+    constructor() {
+        owner = msg.sender; // The owner of the contract
+        cokeBalance[address(this)] = 500; // The balance of the vending machine
+    }
+
+    function getVendingMachineBalance() public view returns(uint) {
+        return cokeBalance[address(this)];
+    }
+
+    function restock(uint amount) public {
+        require(msg.sender == owner, "Only owner can restock this vending machine!!");
+        cokeBalance[address(this)] += amount;
+    }
+
+    function purchase(uint amount) public payable {
+        require(cokeBalance[address(this)] >= amount * 2 ether, "Not enough coke");
+        cokeBalance[msg.sender] += amount; // buyer's balance
+        cokeBalance[address(this)] -= amount; // owner's balance
+    }
+
+}
