7bfff8a4e5a36edd9d134e9e6dc053b57890141c
Keun-Lemur <keunlemur@gmail.com>
2024-11-30 19:30:10 +0900

    ETC: Package related files installed in the project

--- erc20/tutorial_truffle/scripts/big_number.js
+++ erc20/tutorial_truffle/scripts/big_number.js
@@ -1,12 +1,14 @@
-const Web3 = require("web3");
+const { Web3 } = require("web3");
 const web3 = new Web3(
   new Web3.providers.HttpProvider("http://127.0.0.1:9545/")
 );
 
+const BN = require("bn.js");
+
 const decimal = 10 ** 18;
 const exampleNumber = 100000 * decimal;
 const exampleNumber2 = 1000000000000 * decimal;
-const BN = web3.utils.BN;
+// const BN = web3.utils.BN;
 
 Number.prototype.noExponents = function () {
   var data = String(this).split(/[eE]/);
@@ -37,7 +39,7 @@ function getBigNumberEx(exampleNumber) {
   const noExponentsExample2 = new BN(exampleNumber2.noExponents());
   console.log("noExponentsExample2 toString : ", noExponentsExample);
 
-  const addResult = noExponentsExample.add(noExponentsExample2).toString();
+  const addResult = noExponentsExample.add(noExponentsExample2);
   console.log("addResult : ", addResult);
 
   const one_ether = web3.utils.fromWei("1", "ether");
